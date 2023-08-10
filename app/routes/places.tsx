1aa7881f4ae39d9b98acaf6e088e1f128e5b10a4
Pete Cartwright <pete@petecartwright.com>
2023-08-10 07:48:13 -0400

    add filtering

--- app/routes/places.tsx
+++ app/routes/places.tsx
@@ -5,6 +5,7 @@ import { requireUserId } from "~/session.server";
 import { getPlaceListItems } from "~/models/place.server";
 import { buttonVariants } from "@/components/ui/button";
 import { PlaceCard } from "~/components/PlaceCard";
+import { useState } from "react";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const userId = await requireUserId(request);
@@ -16,11 +17,31 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function PlacesPage() {
   const data = useLoaderData<typeof loader>();
 
+  const [placesToDisplay, setPlacesToDisplay] = useState(data.placesListItems);
+
+  const handleSearchChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const searchText = e.currentTarget.value.toLowerCase();
+
+    const filteredPlaces = data.placesListItems.filter((item) => {
+      // return the item if the name or description matches
+      return (
+        item.name.toLowerCase().includes(searchText) ||
+        item.notes.toLowerCase().includes(searchText)
+      );
+    });
+    setPlacesToDisplay(filteredPlaces);
+  };
+
   return (
     <div>
-      {data.placesListItems ? (
+      <input
+        name="search"
+        placeholder="search"
+        onChange={handleSearchChange}
+      ></input>
+      {placesToDisplay ? (
         <ul>
-          {data.placesListItems.map((placeItem) => {
+          {placesToDisplay.map((placeItem) => {
             return (
               <li key={placeItem.id}>
                 <PlaceCard placeItem={placeItem} />
