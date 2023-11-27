982ee651ca7da510d05370c13473682e0f29065c
Pete Cartwright <pete@petecartwright.com>
2023-11-26 21:39:44 -0500

    redirect to /login if not logged in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 import { getUser } from "~/session.server";
 
 export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
@@ -8,31 +7,13 @@ export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
 export const loader = async ({ request }: LoaderArgs) => {
   console.log(request);
   const user = await getUser(request);
-  if (user) return redirect("/places");
-  return null;
+  if (user) {
+    return redirect("/places");
+  } else {
+    return redirect("/login");
+  }
 };
 
 export default function Index() {
-  return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                  <Link
-                    to="/login"
-                    className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-                  >
-                    Log In
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+  return null;
 }
