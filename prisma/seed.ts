95e0c2ab677d81bcc490bc40ab8ab8fecb002e7d
Pete Cartwright <pete@petecartwright.com>
2024-07-08 15:12:37 -0400

    update seed and schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,10 @@ import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
+const SEED_EMAIL = "rachel@remix.run";
+const SEED_PASSWORD = "racheliscool";
+
+
 async function seed() {
   const email = "rachel@remix.run";
 
@@ -11,11 +15,11 @@ async function seed() {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash(SEED_PASSWORD, 10);
 
   const user = await prisma.user.create({
     data: {
-      email,
+      email: SEED_EMAIL,
       password: {
         create: {
           hash: hashedPassword,
@@ -23,21 +27,54 @@ async function seed() {
       },
     },
   });
-
-  await prisma.note.create({
+  
+  const mcDonalds = await prisma.place.create({
     data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
+      name: "McDonald's",
+      userId: user.id
     },
   });
 
-  await prisma.note.create({
+  const mcDonaldsItems = [
+    {name: "EggMcMuffin", note: "peak breakfast"},
+    {name: "Sausage Burrito", note: "does not scratch the itch you want it to"},
+    {name: "Double Quarter pounder with cheese", note: "is perfect once per quarter"},
+    {name: "diet coke", note: "debatably best version of diet coke"},
+  ];
+
+  mcDonaldsItems.forEach(async (item) => {
+    await prisma.item.create({
+      data: {
+        name: item.name,
+        note: item.note,
+        userId: user.id,
+        placeId: mcDonalds.id
+      },
+    });
+  });
+
+  const tacoBell = await prisma.place.create({
     data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
+      name: "Taco Bell",
+      userId: user.id
     },
+  });
+
+  const tacoBellItemsText = [
+    { name: "Chicken Chipotle Ranch Griller", note: "works surprisingly well"},
+    { name: "the bean burrito", note: "is still hard to beat"},
+    { name: "Crunchwrap", note: "usually disappointing but magic when it hits"},
+  ];
+
+  tacoBellItemsText.forEach(async (item) => {
+    await prisma.item.create({
+      data: {
+        name:  item.name,
+        note: item.note,
+        userId: user.id,
+        placeId: tacoBell.id
+      },
+    });
   });
 
   console.log(`Database has been seeded. 🌱`);
